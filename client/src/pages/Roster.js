4dbf15cd07ff6a17093599a82e199bfa6727f893
JaredLove <lovejared91@Yahoo.com>
2023-11-21 21:45:16 -0600

    url

--- client/src/pages/Roster.js
+++ client/src/pages/Roster.js
@@ -181,7 +181,7 @@ function Roster() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`${url}/api/data`);
+      const response = await fetch(`/api/data`);
   
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
